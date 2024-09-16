03c8136a126c3ce05ba4f033c0103f2e3305e289
Brian Siklinski <brian@bsik.net>
2024-09-16 10:31:49 -0400

    refactor to js class, add normalize.css

--- map.js
+++ map.js
@@ -1,99 +1,110 @@
-const contentWrapper = document.querySelector("main");
+class WheelieBabes {
+    constructor() {
+        this.contentWrapper = document.querySelector("main");
+        this.day = new URLSearchParams(window.location.search).get("day"); // only for initial load.
+        this.map = L.map("map").setView([39, -97.5], 4);
 
-window.onload = function () {
-    const day = new URLSearchParams(window.location.search).get("day");
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution:
+                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(this.map);
 
-    if (day) {
-        updateContent(day);
+        this.mapOptions = {
+            async: true,
+            marker_options: {
+                startIconUrl: null,
+                endIconUrl: null,
+            },
+        };
+
+        if (this.day) {
+            this.updateContent(this.day);
+        }
+
+        this.getTracks();
     }
-};
 
-const map = L.map("map").setView([39, -97.5], 4);
+    updateContent(day) {
+        fetch(`assets/json/${day}.json`)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error("Status " + res.status);
+                }
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+                return res.json();
+            })
+            .then((res) => {
+                console.log(res);
+                this.contentWrapper.innerHTML = `<h2>${res.title}</h2>${res.content}`;
+            })
+            .catch((err) => console.log(err));
+    }
 
-const options = {
-    async: true,
-    marker_options: {
-        startIconUrl: null,
-        endIconUrl: null,
-    },
-};
+    getTracks() {
+        fetch("http://127.0.0.1:5000/tracks")
+            .then((res) => (res.ok ? res.json() : false))
+            .then((res) => {
+                res.forEach((track) => {
+                    this.addTrack(track);
+                });
+            });
+    }
 
-fetch("http://127.0.0.1:5000/tracks")
-    .then((res) => (res.ok ? res.json() : false))
-    .then((res) => {
-        res.forEach((track) => {
-            let day = track.split("/");
-            day = Number(day[day.length - 1].split(".")[0]);
+    addTrack(track) {
+        let day = track.split("/");
+        day = Number(day[day.length - 1].split(".")[0]);
 
-            new L.GPX(track, options)
-                .on("loaded", function (e) {
-                    const segment = e.target;
+        new L.GPX(track, this.mapOptions)
+            .on("loaded", (e) => {
+                const segment = e.target;
 
-                    const coordinates =
-                        segment.toGeoJSON().features[0].geometry.coordinates;
-                    start = coordinates[0].reverse();
-                    end = coordinates[coordinates.length - 1].reverse();
+                const coordinates =
+                    segment.toGeoJSON().features[0].geometry.coordinates;
+                const start = coordinates[0].reverse();
+                const end = coordinates[coordinates.length - 1].reverse();
 
-                    L.marker(end)
-                        .bindTooltip(
-                            `📍<code>${segment.get_name().substring(0, 31)}<br />
+                L.marker(end)
+                    .bindTooltip(this.toolTipMarkupEnd(segment, track))
+                    .on("click", () => {
+                        this.setNewParams(day);
+                        this.updateContent(day);
+                    })
+                    .addTo(this.map);
+
+                if (day === 1) {
+                    L.marker(start)
+                        .bindTooltip(this.toolTipMarkupStart(segment, track))
+                        .on("click", () => {
+                            this.setNewParams(day);
+                            this.updateContent(day);
+                        })
+                        .addTo(this.map);
+                }
+            })
+            .addTo(this.map);
+    }
+
+    setNewParams(day) {
+        const url = new URL(window.location);
+        url.searchParams.set("day", day);
+        history.pushState(null, "", url);
+    }
+
+    toolTipMarkupStart(segment, track) {
+        return `📍<code>${segment.get_name().substring(0, 31)}<br />
+                    <- ${track}<br />
+                    ${segment.get_distance_imp().toFixed(2)} miles</code>`;
+    }
+
+    toolTipMarkupEnd(segment, track) {
+        return `📍<code>${segment.get_name().substring(0, 31)}<br />
                     -> ${track}<br />
                     ${segment.get_distance_imp().toFixed(2)} miles<br />
-                    <- ${track}</code>`
-                        )
-                        .on("click", () => {
-                            const url = new URL(window.location);
-                            url.searchParams.set("day", day);
-                            history.pushState(null, "", url);
-
-                            updateContent(day);
-                        })
-                        .addTo(map);
-
-                    if (day === 1) {
-                        L.marker(start)
-                            .bindTooltip(
-                                `📍<code>${segment.get_name().substring(0, 31)}<br />
-                    <- ${track}<br />
-                    ${segment.get_distance_imp().toFixed(2)} miles</code>`
-                            )
-                            .on("click", () => {
-                                const url = new URL(window.location);
-                                url.searchParams.set("day", 1);
-                                history.pushState(null, "", url);
-
-                                updateContent(1);
-                            })
-                            .addTo(map);
-                    }
-                })
-                .addTo(map);
-        });
-    });
-
-function updateContent(day) {
-    fetch(`assets/json/${day}.json`)
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error("Status " + res.status);
-            }
-
-            return res.json();
-        })
-        .then((res) => {
-            console.log(res);
-            contentWrapper.innerHTML = `<h2>${res.title}</h2>${res.content}`;
-        })
-        .catch((err) => console.log(err));
+                    <- ${track}</code>`;
+    }
 }
 
-// each segment has a hover state popup with info about each post relating to it
-//  info to and from: either add to previous marker current segment distance or loop backwards
-
-// on select, will display both blog posts below on desktop and as a full screen modal on mobile?
+window.addEventListener("DOMContentLoaded", () => {
+    new WheelieBabes();
+});
